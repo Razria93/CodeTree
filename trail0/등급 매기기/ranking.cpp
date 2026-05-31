ab1b37179d99e372d914c9d2c4e8a072d5decd6f
Razry <dsyeom931008@gmail.com>
2026-05-31 21:42:24 +0900

    [Passed] 등급 매기기 (2ms, 1MB)

---
+++ trail0/등급 매기기/ranking.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N;
+    std::cin >> N;
+
+    if (N >= 90)
+        std::cout << 'A' << '\n';
+    else if (N >= 80)
+        std::cout << 'B' <<'\n';
+    else if (N >= 70)
+        std::cout << 'C' <<'\n';
+    else if (N >= 60)
+        std::cout << 'D' <<'\n';
+    else
+        std::cout << 'F' << '\n';
+
+    return 0;
+}
