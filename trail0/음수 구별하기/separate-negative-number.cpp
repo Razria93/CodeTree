a613d01aa51d5dae93bc6039fb13396d0878bf4b
Razry <dsyeom931008@gmail.com>
2026-05-31 21:39:51 +0900

    [Passed] 음수 구별하기 (2ms, 0MB)

---
+++ trail0/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    std::cout << N << '\n';
+
+    if (N < 0)
+    {
+        std::cout << "minus" << '\n';
+    }
+
+    return 0;
+}
