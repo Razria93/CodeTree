ff988e4bbf1218fc4b3209076c9f527705822163
Razry <dsyeom931008@gmail.com>
2026-05-31 22:09:27 +0900

    [Passed] 굉장한 수 (2ms, 0MB)

---
+++ trail0/굉장한 수/amazing-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N;
+    std::cin >> N;
+
+    if ((N % 2 != 0 && N % 3 ==0) || (N % 2 == 0 && N % 5 == 0))
+        std::cout << "true";
+    else
+        std::cout << "false";
+
+    return 0;
+}
