7976654e72e14e7ee962b830f212788c4b7f3138
Razry <dsyeom931008@gmail.com>
2026-05-31 21:45:53 +0900

    [Passed] 문자열 출력하기 (2ms, 0MB)

---
+++ trail0/문자열 출력하기/print-string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N;
+    std::cin >> N;
+
+    for (int i = 0; i < N; ++i)
+    {
+        std::cout << "LeebrosCode" << '\n';
+    }
+
+    return 0;
+}
