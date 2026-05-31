63550d9d9be299b0e5d5b9a6a33b06739badf2e6
Razry <dsyeom931008@gmail.com>
2026-05-31 21:44:36 +0900

    [Passed] 문자 출력하기 (2ms, 0MB)

---
+++ trail0/문자 출력하기/print-text.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char n;
+    std::cin >> n;
+
+    for (int i = 0; i < 8; ++i)
+    {
+        std::cout << n;
+    }
+
+    std::cout << '\n';
+
+    return 0;
+}
