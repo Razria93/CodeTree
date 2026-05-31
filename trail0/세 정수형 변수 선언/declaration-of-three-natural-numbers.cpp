d499bf4f4a24218a411961af3b4db4b1e93fd0e6
Razry <dsyeom931008@gmail.com>
2026-05-31 21:29:08 +0900

    [Passed] 세 정수형 변수 선언 (1ms, 0MB)

---
+++ trail0/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a = 7;
+    int b = 23;
+    int c = 30;
+
+    std::cout << a << " + " << b << " = " << a + b << '\n';
+
+    return 0;
+}
