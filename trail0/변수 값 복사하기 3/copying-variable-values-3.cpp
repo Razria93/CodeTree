d5c4821527188272b638d16a258698afdb2f6537
Razry <dsyeom931008@gmail.com>
2026-05-31 21:27:59 +0900

    [Passed] 변수 값 복사하기 3 (2ms, 0MB)

---
+++ trail0/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a = 1;
+    int b = 5;
+    int c = 3;
+
+    a = c;
+    a += c;
+    b -= c;
+
+    std::cout << a << '\n' << b << '\n' << c << '\n';
+
+    return 0;
+}
