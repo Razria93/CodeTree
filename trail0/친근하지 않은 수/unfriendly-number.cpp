ef450b99b61d654f1f4dc09c4155d3fbb7421988
Razry <dsyeom931008@gmail.com>
2026-06-03 10:58:46 +0900

    [Passed] 친근하지 않은 수 (2ms, 0MB)

---
+++ trail0/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    int count = 0;
+    for (int i = 1; i <= N; ++i)
+    {
+        if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) continue;
+
+        ++count;
+    }
+
+    std::cout << count;
+
+    return 0;
+}
