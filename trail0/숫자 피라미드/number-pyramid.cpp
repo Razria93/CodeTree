9a2f2e906f9f970965ec166206c7116d3bfd1604
Razry <dsyeom931008@gmail.com>
2026-06-03 22:17:47 +0900

    [Passed] 숫자 피라미드 (2ms, 0MB)

---
+++ trail0/숫자 피라미드/number-pyramid.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    for (int i = 1; i <= N; ++i)
+    {
+        for (int j = 1; j <= i; ++j)
+        {
+            std::cout << i << ' ';
+        }
+
+        std::cout << '\n';
+    }
+
+    return 0;
+}
