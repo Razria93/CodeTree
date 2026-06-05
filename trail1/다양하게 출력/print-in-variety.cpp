9208af83b0db0ac9a3c4883ec3c15e2f4695f622
Razry <dsyeom931008@gmail.com>
2026-06-05 20:42:43 +0900

    [Passed] 다양하게 출력 (2ms, 0MB)

---
+++ trail1/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout.precision(10);
+    std::cout << std::fixed;
+
+    std::cout << "Total days in Year" << '\n';
+    std::cout << 365 << '\n';
+    std::cout << "Circumference rate" << '\n';
+    std::cout << 3.1415926535 << '\n';
+    return 0;
+}
