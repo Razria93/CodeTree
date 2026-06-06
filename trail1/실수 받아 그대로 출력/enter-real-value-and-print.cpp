c469f6eec8a2506364b6ec73f4999a0f3497f099
Razry <dsyeom931008@gmail.com>
2026-06-06 14:33:21 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 0MB)

---
+++ trail1/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float N;
+    std::cin >> N;
+
+    std::cout << std::fixed;
+    std::cout.precision(2);
+
+    std::cout << N;
+
+    return 0;
+}
