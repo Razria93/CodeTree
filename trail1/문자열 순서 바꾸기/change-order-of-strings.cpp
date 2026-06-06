2a7c014b18eb6ec1bf5ce31805538ce571514dea
Razry <dsyeom931008@gmail.com>
2026-06-06 14:55:05 +0900

    [Passed] 문자열 순서 바꾸기 (2ms, 0MB)

---
+++ trail1/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    std::string S;
+    std::string T;
+
+    std::cin >> S >> T;
+
+    std::cout << T << '\n';
+    std::cout << S << '\n';
+
+    return 0;
+}
