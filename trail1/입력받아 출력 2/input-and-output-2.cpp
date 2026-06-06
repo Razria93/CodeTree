89288711aee3f2c0ede687c407dd087bfe1a8bd9
Razry <dsyeom931008@gmail.com>
2026-06-06 18:08:42 +0900

    [Passed] 입력받아 출력 2 (2ms, 0MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int f, b;
+    std::cin >> f;
+    std::cin.get();
+    std::cin >> b;
+
+    std::cout << f << b;
+
+    return 0;
+}
