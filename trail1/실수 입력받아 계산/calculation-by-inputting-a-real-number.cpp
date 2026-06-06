ec3ea4b755f1897b0a31fdb00692b5af53d1ba62
Razry <dsyeom931008@gmail.com>
2026-06-06 14:47:27 +0900

    [Passed] 실수 입력받아 계산 (2ms, 0MB)

---
+++ trail1/실수 입력받아 계산/calculation-by-inputting-a-real-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double a, b;
+    std::cin >> a >> b;
+
+    std::cout << std::fixed;
+    std::cout.precision(2);
+
+    std::cout << a + b;
+
+    return 0;
+}
