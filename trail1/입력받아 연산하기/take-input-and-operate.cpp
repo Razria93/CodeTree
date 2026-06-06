76b421013bb08fcadba60df33739eae0accc42c9
Razry <dsyeom931008@gmail.com>
2026-06-06 18:21:14 +0900

    [Passed] 입력받아 연산하기 (2ms, 0MB)

---
+++ trail1/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    std::cin >> a >> b;
+
+    std::cout << a + 87 << '\n';
+    std::cout << b % 10 << '\n';
+
+    return 0;
+}
