211a278f667e1a7bdd2bcd4921a81f74b0aebde4
Razry <dsyeom931008@gmail.com>
2026-06-06 12:43:48 +0900

    [Passed] 변수 값 교체하기 6 (1ms, 0MB)

---
+++ trail1/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 2, b = 5;
+    std::swap(a, b);
+    std::cout << a << '\n';
+    std::cout << b << '\n';
+
+    return 0;
+}
