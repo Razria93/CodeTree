3a71d24e171ba6849f547672476c792a6cda76db
Razry <dsyeom931008@gmail.com>
2026-06-10 15:40:14 +0900

    [Passed] 정수의 조건 여부 (2ms, 0MB)

---
+++ trail1/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a;
+    std::cin >> a;
+
+    if (a >= 113)
+        std::cout << 1 << '\n';
+    else
+        std::cout << 0 << '\n';
+
+    return 0;
+}
