73f66ab77d6abb793ea1f7e2cd6772688e54c2f9
Razry <dsyeom931008@gmail.com>
2026-06-10 17:52:36 +0900

    [Passed] 물의 상태 (2ms, 0MB)

---
+++ trail1/물의 상태/state-of-water.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    std::cin >> n;
+
+    if (n < 0)
+        std::cout << "ice" <<'\n';
+    else if (n >= 100)
+        std::cout << "vapor" << '\n';
+    else
+        std::cout << "water" << '\n';
+
+    return 0;
+}
