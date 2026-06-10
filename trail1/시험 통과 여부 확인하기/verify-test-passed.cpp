8055b9ecc5c053bcaa3ba13bca8b5148f1e64422
Razry <dsyeom931008@gmail.com>
2026-06-10 15:36:52 +0900

    [Passed] 시험 통과 여부 확인하기 (2ms, 0MB)

---
+++ trail1/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N;
+    std::cin >> N;
+
+    if (N >= 80)
+        std::cout << "pass" << '\n';
+    else
+        std::cout << 80 - N <<  " more score" << '\n';
+
+    return 0;
+}
