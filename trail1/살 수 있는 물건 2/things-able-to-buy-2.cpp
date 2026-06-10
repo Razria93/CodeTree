05230ac194d98ae1e1cc64ece238741cf77ee03d
Razry <dsyeom931008@gmail.com>
2026-06-10 21:25:41 +0900

    [Passed] 살 수 있는 물건 2 (2ms, 0MB)

---
+++ trail1/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int N;
+    std::cin >> N;
+
+    if (N >= 3000)
+        std::cout << "book" << '\n';
+    else if (N >= 1000)
+        std::cout << "mask" << '\n';
+    else if (N >= 500)
+        std::cout << "pen" << '\n';
+    else
+        std::cout << "no" << '\n';
+
+    return 0;
+}
